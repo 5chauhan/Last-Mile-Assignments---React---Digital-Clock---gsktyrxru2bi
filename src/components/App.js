8686fce5476199a64b6ffa6333a86fa73a909638
Sourabh Chauhan <97791303+5chauhan@users.noreply.github.com>
2022-04-26 15:41:20 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,54 +1,26 @@
-import React, { Component, useState } from "react";
-import "../styles/App.css";
+import React, { Component } from 'react';
+import './App.css'
 
-class App extends Component {
-  constructor() {
-    super();
-    let date = new Date();
-      let hours = date.getHours();
-      let minutes = date.getMinutes();
-      let seconds = date.getSeconds();
-      let ampm = hours >= 12 ? "PM" : "AM";
-      hours = hours % 12;
-      hours = hours ? hours : 12; // the hour '0' should be '12'
-      minutes = minutes < 10 ? "0" + minutes : minutes;
-      seconds = seconds < 10 ? "0" + seconds : seconds;
-      let strTime = hours + ":" + minutes + ":" + seconds +" "+ ampm;
-    this.state = { time: strTime };
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {time: new Date().toLocaleTimeString()};
   }
-  componentDidMount() {
-    console.log("didMount");
-    this.intId = setInterval(() => {
-      let date = new Date();
-      let hours = date.getHours();
-      let minutes = date.getMinutes();
-      let seconds = date.getSeconds();
-      let ampm = hours >= 12 ? "PM" : "AM";
-      hours = hours % 12;
-      hours = hours ? hours : 12; // the hour '0' should be '12'
-      minutes = minutes < 10 ? "0" + minutes : minutes;
-      seconds = seconds < 10 ? "0" + seconds : seconds;
-      let strTime = hours + ":" + minutes + ":" + seconds +" "+ ampm;
-      
-      this.setState({ time: strTime });
-    }, 1000);
+  componentDidMount(){
+    this.timerID = setInterval(
+      () => this.tick(), 1000
+    )
   }
-
-  
+  tick() {
+    this.setState({time: new Date().toLocaleTimeString()});
+  }
   componentWillUnmount() {
-    
-    clearInterval(this.intId);
+    clearInterval(this.timerID);
   }
   render() {
     return (
-      <div className="App">
-        <div className="Clock">
-          <h3 id="time">{`${this.state.time}`}</h3>
-        </div>
-      </div>
-    );
+        <div className='Clock'> <h3 id='time'>{this.state.time}</h3> </div>
+    )
   }
 }
 
-export default App;
-export default App;
