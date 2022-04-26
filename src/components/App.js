76abe1bc73816790a70ddcbcec6e267ab5b30881
Sourabh Chauhan <97791303+5chauhan@users.noreply.github.com>
2022-04-27 00:32:43 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,31 @@
-import React, { Component } from 'react';
-import './App.css'
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
-export default class App extends Component {
-  constructor(props){
+class App extends Component {
+  constructor(props) {
     super(props);
-    this.state = {time: new Date().toLocaleTimeString()};
+    this.state = {
+      date: new Date()
+    };
   }
-  componentDidMount(){
-    this.timerID = setInterval(
-      () => this.tick(), 1000
-    )
-  }
-  tick() {
-    this.setState({time: new Date().toLocaleTimeString()});
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
   }
   componentWillUnmount() {
     clearInterval(this.timerID);
   }
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
   render() {
     return (
-        <div className='Clock'> <h3 id='time'>{this.state.time}</h3> </div>
-    )
+      <div className="Clock">
+        <h3 id="time">{this.state.date.toLocaleTimeString()}</h3>
+      </div>
+    );
   }
 }
 
+export default App;
