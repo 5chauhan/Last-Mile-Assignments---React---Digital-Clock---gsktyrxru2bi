21e2276961872ab1faa35b37cef432879f561915
Sourabh Chauhan <97791303+5chauhan@users.noreply.github.com>
2022-05-05 04:25:15 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./components/App";
+// function generateCodeFromObject(obj) {
+//   //return a code generated string
+// }
+
+// module.exports = generateCodeFromObject;
+// ReactDOM.render(<App />, document.getElementById("root"));
+
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-function generateCodeFromObject(obj) {
-  //return a code generated string
-}
+// function generateCodeFromObject(obj){
+//     //return a code generated string
+//    }
+   
+// module.exports=generateCodeFromObject;
 
-module.exports = generateCodeFromObject;
+
 ReactDOM.render(<App />, document.getElementById("root"));
